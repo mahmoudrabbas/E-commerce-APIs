4b9ec65161f73fe3cf758d3f44bf285f2d39fe9d
Mahmoud R Abbas <88283393+mahmoudrabbas@users.noreply.github.com>
2022-07-06 06:29:05 +0200

    second commit

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,17 +1,38 @@
 const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
-    let data;
-    const token = req.headers.token;
-    if(token){
+    if(req.headers.token){
+        const token = req.headers.token.split(" ")[1];
         jwt.verify(token, process.env.JWT_SEC_KEY, ( err, user ) => {
-            if(err) res.status(403).json({ message: "Token is not valid" })
+            if(err) return res.status(403).json({ message: "token is not valid" })
             req.user = user;
-            next
+            if(req.user.id === req.params.id || req.user.isAdmin) {
+                next()
+            } else {
+                res.status(403).json({ message: "not allowed to do that" })
+            }
         })
     }else{
-        return res.status(401).json({ message: "Unauthenticated" })
+        return res.status(401).json({ message: "not signed in" })
     }
 }
 
-module.exports = { auth }
+const isAdminAuth = (req, res, next) => {
+    if(req.headers.token){
+        const token = req.headers.token.split(" ")[1];
+        jwt.verify(token, process.env.JWT_SEC_KEY, ( err, user ) => {
+            if(err) return res.status(403).json({ message: "token is not valid" })
+            req.user = user;
+            if(req.user.isAdmin) {
+                next()
+            } else {
+                res.status(403).json({ message: "not admin" })
+            }
+        })
+    }else{
+        return res.status(401).json({ message: "not signed in" })
+    } 
+}
+
+
+module.exports = { auth, isAdminAuth }
